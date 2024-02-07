89bc2133a676131c259583f14eb35c3e6e658e97
zwa73 <ssfen2419@126.com>
2024-02-07 10:21:42 +0800

    format

--- src/CharBuild/UtilGener.ts
+++ src/CharBuild/UtilGener.ts
@@ -4,31 +4,40 @@ import { MutationID } from "cdda-schema";
 
 
 /**获取角色变异ID */
-export const getCharMutId = (charName:string)=>`${charName}_cnpc` as MutationID;
+export const getCharMutId = (charName:string) =>
+	`${charName}_cnpc` as MutationID;
 
 /**获取职业ID */
-export const getCharClassId = (charName:string)=>CMDef.genNpcClassID(`${charName}_NpcClassID`);
+export const getCharClassId = (charName:string) =>
+	CMDef.genNpcClassID(`${charName}_NpcClassID`);
 
 /**获取实例ID */
-export const getCharInstanceId = (charName:string)=>CMDef.genNpcInstanceID(`${charName}_NpcInstanceID`);
+export const getCharInstanceId = (charName:string) =>
+	CMDef.genNpcInstanceID(`${charName}_NpcInstanceID`);
 
 /**获取基础装甲ID */
-export const getCharBaseArmorId = (charName:string)=>CMDef.genArmorID(`${charName}_ArmorID`);
+export const getCharBaseArmorId = (charName:string) =>
+	CMDef.genArmorID(`${charName}_ArmorID`);
 
 /**获取基础附魔ID */
-export const getCharBaseEnchId = (charName:string)=>CMDef.genEnchantmentID(`${charName}_EnchantmentID`);
+export const getCharBaseEnchId = (charName:string) =>
+	CMDef.genEnchantmentID(`${charName}_EnchantmentID`);
 
 /**获取基础物品标志ID */
-export const getCharBaseItemFlagId = (charName:string)=>CMDef.genFlagID(`${charName}_WeaponFlag_FlagID`);
+export const getCharBaseItemFlagId = (charName:string) =>
+	CMDef.genFlagID(`${charName}_WeaponFlag_FlagID`);
 
 /**获取基础携带组 */
-export const getCharBaseCarryGroup = (charName:string)=>CMDef.genItemGroupID(`${charName}_Carry_ItemGroupID`);
+export const getCharBaseCarryGroup = (charName:string) =>
+	CMDef.genItemGroupID(`${charName}_Carry_ItemGroupID`);
 
 /**获取对话主题ID */
-export const getCharTalkTopicId = (charName:string)=>CMDef.genTalkTopicID(`${charName}_TalkTopicID`);
+export const getCharTalkTopicId = (charName:string) =>
+	CMDef.genTalkTopicID(`${charName}_TalkTopicID`);
 
 /**获取卡片ID */
-export const getCharCardId = (charName:string)=>CMDef.genGenericID(`${charName}_Card_GenericID`);
+export const getCharCardId = (charName:string) =>
+	CMDef.genGenericID(`${charName}_Card_GenericID`);
 
 
 /**获取全局的强化字段的变量ID */
@@ -40,4 +49,5 @@ export function getTalkerFieldVarID(talker:"u"|"n",field:string){
 }
 
 /**获取角色展示名 */
-export const getCharDisplayName = (charName:string)=>charName.replaceAll("_"," ");
+export const getCharDisplayName = (charName:string) =>
+	charName.replaceAll("_"," ");
